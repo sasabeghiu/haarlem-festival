24a75b25e95910625dd72728fe4aacdb514af655
Sasha <646415@student.inholland.nl>
2023-03-15 12:34:59 +0100

    fix navbars

---
+++ app/views/visithaarlem/festivalhomepage.php
@@ -0,0 +1,113 @@
+<?php
+//incl new navbar
+include __DIR__ . '/../header.php';
+?>
+
+<div style="position: relative; text-align: center; color: white;" class="click2edit">
+    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($page->getHeaderImg()); ?>" width="100%" height="auto">
+    <div style="position: absolute; bottom: 8px; left: 16px;">
+        <h4 style="display:inline;">
+            </h5>
+            <h1 style="display:inline;" class="click2edit"> <?= $page->getTitle() ?></h1>
+    </div>
+</div>
+<!-- Main Content -->
+<div class="container mb-3">
+    <!-- <div class="row mb-4">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="your-image-url-here" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card description goes here...</p>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <div class="row mb-3" style="display:flex; justify-content:center;">
+        <?php
+        foreach ($pagecards as $card) {
+        ?>
+            <div class="col">
+                <div class="card">
+                    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($card->getImage()); ?>" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="text-center fw-bold click2edit"><?= $card->getTitle() ?></h5>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+</div>
+<div class="container mb-3">
+    <div class="card" style="width: 75%;">
+        <div class="row">
+            <div class="col-md-9">
+                <img src="https://www.haarlemjazzandmore.nl/wp-content/uploads/2019/08/20190818_jazz_vuurwerk_07-1024x683.jpg" class="card-img rounded-left" alt="...">
+            </div>
+            <div class="col-md-3 bg-light rounded-right">
+                <div class="card-body text-center">
+                    <h5 class="card-title">The Haarlem Festival</h5>
+                    <p class="card-text">Amazing artists, great DJs and much more... </br> All in the beautiful beach of Haarlem</p>
+                    <a href="/page/festivalpage" class="btn btn-primary">CHECK IT OUT</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="container mb-3">
+    <div class="">
+        <p class="click2edit"> <?= $page->getDescription() ?></p>
+    </div>
+</div>
+
+
+
+<!-- Location -->
+<div class="container mb-3">
+    <div class="row-md-6">
+        <div class="location">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.660308624696!2d4.632323215799311!3d52.386532879785394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6097df89360c9%3A0x1a70afdd12fd6e39!2sGrote%20Markt%2C%20Haarlem!5e0!3m2!1sen!2snl!4v1615301111915!5m2!1sen!2snl" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+    <div class="col-md-6 mb-3">
+        <h3>Did You Know?</h3>
+        <ul>
+            <li>Random Haarlem fact 1</li>
+            <li>Random Haarlem fact 2</li>
+            <li>Random Haarlem fact 3</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container mb-3">
+    <h4>Latest posts from #haarlem</h4>
+    <div class="row mt-2">
+        <div class="col-12">
+            <iframe src="https://widget.taggbox.com/124424" style="width:100%;height:600px;border:none;"></iframe>
+        </div>
+    </div>
+</div>
+
+<script>
+    var edit = function() {
+        $(".click2edit").each(function() {
+            $(this).summernote({
+                focus: true,
+            });
+        });
+    };
+
+    var save = function() {
+        $(".click2edit").each(function() {
+            $(this).summernote("code");
+            $(this).summernote("destroy");
+        });
+    };
+</script>
+<?php
+include __DIR__ . '/../footer.php';
+?>
