5b66e0ed201ecdf80534c67f087830565250fd89
Pablo Gulias <pabloguliasprats@gmail.com>
2023-03-27 12:44:36 +0200

    install mollie

--- app/vendor/composer/autoload_static.php
+++ app/vendor/composer/autoload_static.php
@@ -31,39 +31,20 @@ class ComposerStaticInit5be3b990ee4a7f84cef6ed827e40f573
         array(
             'BaconQrCode\\' => 12,
         ),
-        'P' =>
-        array(
-    public static $prefixLengthsPsr4 = array (
-        'P' => 
-        array (
-            'PHPMailer\\PHPMailer\\' => 20,
-        ),
-        'M' =>
-        array(
-            'Mollie\\Api\\' => 11,
-        ),
-        'C' =>
-        array(
-            'Composer\\CaBundle\\' => 18,
-        ),
     );
 
-    public static $prefixDirsPsr4 = array (
-        'PHPMailer\\PHPMailer\\' => 
-        array (
+    public static $prefixDirsPsr4 = array(
+        'PHPMailer\\PHPMailer\\' =>
+        array(
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
-        'Mollie\\Api\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/mollie/mollie-api-php/src',
-        ),
-        'Composer\\CaBundle\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/composer/ca-bundle/src',
+        'Firebase\\JWT\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
         ),
     );
 
-    public static $classMap = array (
+    public static $classMap = array(
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Datamatrix' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/barcodes/datamatrix.php',
         'PDF417' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/barcodes/pdf417.php',
@@ -87,7 +68,6 @@ class ComposerStaticInit5be3b990ee4a7f84cef6ed827e40f573
             $loader->prefixLengthsPsr4 = ComposerStaticInit5be3b990ee4a7f84cef6ed827e40f573::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit5be3b990ee4a7f84cef6ed827e40f573::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit5be3b990ee4a7f84cef6ed827e40f573::$classMap;
-
         }, null, ClassLoader::class);
     }
 }
