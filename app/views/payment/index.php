44ccf0a0df07b470f922f44d428123e9d34565f2
Pablo Gulias <pabloguliasprats@gmail.com>
2023-03-29 00:45:20 +0200

    payment WIP, need order methods, to save payment

--- app/views/payment/index.php
+++ app/views/payment/index.php
@@ -34,23 +34,21 @@
                             </div>
                             <div class="mb-3">
                                 <label for="issuer" class="form-label">Issuer</label>
-                                <select name="issuer" class="form-control">
-                                    <?php foreach ($method->issuers() as $issuer) {
-                                        echo "<option value='$issuer->id'>$issuer->name</option>";
-                                    }
-                                    ?>
+                                <select class="form-control" name="issuer">
+                                    <?php foreach ($method->issuers() as $issuer) { ?>
+                                        <option value="<?php echo htmlspecialchars($issuer->name) ?>"> </option>
+                                    <?php } ?>
                                     <option value="">or select later</option>
                                 </select>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col">
-                                    <button type="submit" class="btn btn-primary">Pay</button>
-                                </div>
-                            </div>
                         </form>
                     </div>
+                    <div class="row mb-3">
+                        <div class="col"><button type="submit" class="btn btn-primary">Pay</button></div>
+                    </div>
+                    </form>
                 </div>
             </div>
+        </div>
         </div>
         </div>
 
@@ -63,5 +61,3 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
-
-</html>
