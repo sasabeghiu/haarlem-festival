57d2e97ba0c7e3205c9630a2f7f95d8dc7dd6a5c
DennisDoksani <70758915+DennisDoksani@users.noreply.github.com>
2023-03-29 10:02:48 +0200

    historyevent index page fixed

--- app/views/historyevent/index.php
+++ app/views/historyevent/index.php
@@ -64,9 +64,10 @@ include __DIR__ . '/../header.php';
                         ?>
                             <h4 class="text-danger fw-bold">Only <?= ucfirst($historyevent->getTicketsAvailable()) ?> tickets left!</h4>
                             <div class="card-button">
+                                <a class="btn btn-info" href="#">Book for <?= $historyevent->getPrice() ?>€</a>
                                 <form action="/historyevent" method="post">
-                                    <button class="btn btn-info" name="add-to-cart">Add to cart for <?= $historyevent->getPrice() ?>€</button>
-                                    <input type="hidden" name="product_id" value="<?= $historyevent->getProduct_id() ?>">
+                                    <button class="btn btn-secondary" name="add-to-cart">Add to cart</button>
+                                    <input type="hidden" name="product_id" value="<?= $historyevent->getId() ?>">
                                 </form>
                             </div>
                         <?php
@@ -74,9 +75,10 @@ include __DIR__ . '/../header.php';
                         ?>
                             <h4 class="text-success fw-bold">Seats available: <?= ucfirst($historyevent->getTicketsAvailable()) ?></h4>
                             <div class="card-button col-xs-1">
+                                <a class="btn btn-info" href="#">Book for <?= $historyevent->getPrice() ?>€</a>
                                 <form action="/historyevent" method="post">
-                                    <button class="btn btn-info" name="add-to-cart">Add to cart for <?= $historyevent->getPrice() ?>€</button>
-                                    <input type="hidden" name="product_id" value="<?= $historyevent->getProduct_id() ?>">
+                                    <button class="btn btn-secondary" name="add-to-cart">Add to cart</button>
+                                    <input type="hidden" name="product_id" value="<?= $historyevent->getId() ?>">
                                 </form>
                             </div>
                         <?php
