949ea3fc8aaa6221b8137bbe942704dcb166400b
Pablo Gulias <pabloguliasprats@gmail.com>
2023-04-02 13:02:24 +0200

    added pdf, creating

--- app/controllers/paymentcontroller.php
+++ app/controllers/paymentcontroller.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 
+require_once __DIR__ . '../vendor/tecnickcom/tcpdf/tcpdf.php';
+
+
 class PaymentController
 {
     //private $orderService;
@@ -13,21 +16,14 @@ class PaymentController
         test_Ds3fz4U9vNKxzCfVvVHJT2sgW5ECD8");
     }
 
-    public function index()
-    {
-        $method = $this->mollie->methods->get(\Mollie\Api\Types\PaymentMethod::IDEAL, ["include" => "issuers"]);
-        require __DIR__ . '/../views/payment/index.php';
-    }
-
     public function pay()
     {
         try {
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $amount = $_POST['amount'];
-                $formatted_amount = number_format((float)$amount, 2, '.', '');
-                $orderId = time();
+            $amount = $_POST['amount'];
+            $formatted_amount = number_format((float)$amount, 2, '.', '');
+            $orderId = time();
 
-                /*
+            /*
              * Payment parameters:
              *   amount        Amount in EUROs. This example creates a € 10,- payment.
              *   description   Description of the payment.
@@ -35,20 +31,18 @@ class PaymentController
              *   webhookUrl    Webhook location, used to report when the payment changes state.
              *   metadata      Custom metadata that is stored with the payment.
              */
-                $payment = $this->mollie->payments->create([
-                    "amount" => [
-                        "currency" => "EUR",
-                        "value" => (string)$formatted_amount,
-                    ],
-                    "description" => "Order #{orderId}",
-                    "redirectUrl" => "http://localhost\payment\status?orderId=1",
-                    "webhookUrl" => "http://localhost\payment\webhook",
-                    "metadata" => [
-                        "order_id" => 1,
-                    ],
-                    "issuer" => !empty($_POST["issuer"]) ? $_POST["issuer"] : null,
-                ]);
-            }
+            $payment = $this->mollie->payments->create([
+                "amount" => [
+                    "currency" => "EUR",
+                    "value" => (string)$formatted_amount,
+                ],
+                "description" => "Order #{orderId}",
+                "redirectUrl" => "http://localhost\payment\status?orderId=1",
+                "webhookUrl" => "http://localhost\payment\webhook",
+                "metadata" => [
+                    "order_id" => 1,
+                ],
+            ]);
 
             //$this->orderService->saveStatus($orderId, $payment->status);
 
@@ -59,7 +53,6 @@ class PaymentController
     }
     public function status()
     {
-        echo "payment complete" . "<br><a href='/payment'>Return</a>";
         //$status = $this->orderService->getStatus($_GET["order_id"]));
 
 
@@ -99,5 +92,55 @@ class PaymentController
         } catch (\Mollie\Api\Exceptions\ApiException $e) {
             echo "API call failed: " . htmlspecialchars($e->getMessage());
         }
+    }
+
+    public function createInvoice()
+    {
+        // create new PDF document
+        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('Your Name');
+        $pdf->SetTitle('Invoice');
+        $pdf->SetSubject('Invoice');
+
+        // set default header data
+        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, 'Invoice', 'Your Company', array(0, 64, 255), array(0, 64, 128));
+
+        // set header and footer fonts
+        $pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+        // set auto page breaks
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        // set some language-dependent strings (optional)
+        if (@file_exists(dirname(__FILE__) . '/lang/eng.php')) {
+            require_once(dirname(__FILE__) . '/lang/eng.php');
+            $pdf->setLanguageArray($l);
+        }
+
+        // add a page
+        $pdf->AddPage();
+
+        // write the HTML content into the PDF
+        $html = '<h1>Invoice</h1>';
+        $html .= '<p>Here is the invoice content...</p>';
+        $pdf->writeHTML($html, true, false, true, false, '');
+
+        // output the PDF as a file (you can also send it to a browser or save to a server)
+        $pdf->Output('invoice.pdf', 'D');
     }
 }
--- app/vendor/composer/installed.php
+++ app/vendor/composer/installed.php
@@ -46,5 +46,14 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.6.2',
+            'version' => '6.6.2.0',
+            'reference' => 'e3cffc9bcbc76e89e167e9eb0bbda0cab7518459',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
 );
