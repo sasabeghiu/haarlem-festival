798ae1a9e6cf0105e809bf6fde28d3f99c69134c
DennisDoksani <70758915+DennisDoksani@users.noreply.github.com>
2023-04-10 19:49:40 +0200

    My Orders page funtionalities and design finished.

--- app/views/orders/myorders.php
+++ app/views/orders/myorders.php
@@ -64,30 +64,38 @@ include __DIR__ . '/../header.php';
         <h2 class="text-center">My Orders</h2>
     </div>
     <?php
-        $ordercount = 1;
-        foreach ($myOrders as $myOrder) {
-
-            // foreach($orderStatus as $status){
-                
-            // }
-
-            // $paymnet = $this->mollie->payments->get()
+    $ordercount = 1;
+    foreach ($myOrders as $myOrder) {
     ?>
-            <ul class="list-group mb-3">
-                <li class="list-group-item d-flex justify-content-between lh-sm">
-                    <div class="col-md-6">
-                        <h5 class="mb1">Order Item: <?= $ordercount ?></h5>
-                        <small class="pt-2">
-                            <p>Event name: <?= $myOrder->getEvent_name() ?></p>
-                            <p> Qty: <?= $myOrder->getQty() ?></p>
-                        </small>
-                    </div>
-                    <span class="muted">Total: &euro;<?= $myOrder->getEvent_price() ?></span>
-                </li>
-        <?php
-            $ordercount++;
-        }
-        ?>
+        <ul class="list-group mb-3">
+            <li class="list-group-item d-flex justify-content-between lh-sm">
+                <div class="col-md-6">
+                    <h5 class="mb1">Order Item: <?= $ordercount ?></h5>
+                    <small class="pt-2">
+                        <p>Event name: <?= $myOrder->getEvent_name() ?></p>
+                        <p> Qty: <?= $myOrder->getQty() ?></p>
+                    </small>
+                </div>
+                <span class="muted">Total: &euro;<?= $myOrder->getEvent_price() ?></span>
+                <?php
+                $foundPaymentStatus = false;
+                foreach ($orderStatus as $status) {
+                    $paymnet = $this->mollie->payments->get($status->getPaymentId());
+                    $status = $paymnet->status;
+                    if ($status && !$foundPaymentStatus) {
+                        $foundPaymentStatus = true;
+                ?>
+                        <span class="muted">Payment status: <?= $status ?></span>
+                <?php
+                    }
+                }
+                ?>
+            </li>
+        </ul>
+    <?php
+        $ordercount++;
+    }
+    ?>
 </div>
 
 <?php
